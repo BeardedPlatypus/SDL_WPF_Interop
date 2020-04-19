1fba3821aab577d76b62e7ef02f3b41f576e7299
BeardedPlatypus <mwtegelaers@gmail.com>
2020-04-19 20:49:11 +0200

    Connect SdlHost

--- InteropSDL.App.WPF/MainWindow.xaml.cs
+++ InteropSDL.App.WPF/MainWindow.xaml.cs
@@ -20,9 +20,17 @@ namespace InteropSDL.App.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SdlHost _sdlHost;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void On_UIReady(object sender, EventArgs e)
+        {
+            _sdlHost = new SdlHost(SDLHostElement.ActualHeight, SDLHostElement.ActualWidth);
+            SDLHostElement.Child = _sdlHost;
         }
     }
 }
